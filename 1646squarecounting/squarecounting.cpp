32f6a0264a3cefdb11eaf98a07a7c8f3c3decb70
Inferno <43pradeep07@gmail.com>
2022-03-04 23:23:30 +0530

    1646 Square Counting

---
+++ 1646squarecounting/squarecounting.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n, s;
+        cin>>n>>s;
+        cout<< s/(n*n) <<endl;
+    }
+    
+    return 0;
+}

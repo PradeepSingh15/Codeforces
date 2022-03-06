b16cbd87eb86487d6e1f84adff31ba062e29a16a
Inferno <43pradeep07@gmail.com>
2022-03-06 14:30:32 +0530

    Previously done Questions

---
+++ 158-A-Next Round/nextround.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k, a, b = 0, mas[10000];
+
+int main()
+{
+    cin>>n>>k;
+    for (int i = 1; i <= n; i++) {
+        cin >> a;
+        mas[i] = a;
+    }
+    for (int j = 1; j <= n; j++) 
+    {
+        if (mas[j] >= mas[k] && mas[j] > 0) 
+        {
+            b++;
+        }
+    }
+    cout<<b;
+    
+    return 0;
+}
---
+++ 96-A-Football/football.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int c = 1;
+    for(int i=1; i<=s.length(); i++)
+    {
+        if(s[i] == s[i-1])
+        {
+            c++;
+            if(c == 7)
+            {
+                cout<<"YES"<<endl;
+                return 0;
+            }
+        }
+        else
+          c = 1;
+        
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}

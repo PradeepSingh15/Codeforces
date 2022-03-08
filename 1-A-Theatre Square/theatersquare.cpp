a1ebb7910d381911e813d1a457fe09f0cb966a7e
Inferno <43pradeep07@gmail.com>
2022-03-09 00:01:28 +0530

    Theatre Square

---
+++ 1-A-Theatre Square/theatersquare.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n, m, a, res;
+    cin>>n>>m>>a;
+    res = n % a == 0 ? n/a : n/a + 1;
+    res = res * (m % a == 0 ? m/a : m/a + 1);
+    cout<<res<<endl;
+
+    return 0;
+}

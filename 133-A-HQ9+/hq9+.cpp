0d99d050232673b7fd9028a26b0a459d2fc93191
Inferno <43pradeep07@gmail.com>
2022-03-17 23:53:35 +0530

    HQ9+

---
+++ 133-A-HQ9+/hq9+.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string s;
+   cin>>s;
+   for(int i=0; i<s.size(); i++)
+   {
+       if((s[i] >= 33) && (s[i] <= 126))
+       {
+           if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+           {
+               cout<<"YES"<<endl;
+               return 0;
+           }
+           else
+               continue;
+       }
+   }
+   cout<<"NO"<<endl;
+   
+   return 0;
+}

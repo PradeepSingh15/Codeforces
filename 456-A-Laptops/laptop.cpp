03c6fa282a77829cdcbc848bcdf537a4f92440ab
Inferno <43pradeep07@gmail.com>
2022-04-08 14:02:43 +0530

    Laptop

---
+++ 456-A-Laptops/laptop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int a, b;
+        cin>>a>>b;
+        if(a != b){
+           cout<<"Happy Alex"<<endl;
+           return 0;
+        }
+    }
+    cout<<"Poor Alex"<<endl;
+    return 0;
+}

98fe704ba4fdf11a5c9dc4b22ed21b3f0672b02e
Inferno <43pradeep07@gmail.com>
2022-04-08 13:40:14 +0530

    Little Elephant Bits

---
+++ 258-A-Little Elephant and Bits/littleelephantandbits.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int c = s.find('0');
+    if (c == -1)
+        cout<<s.substr(1)<<endl;
+    else
+        cout<<s.substr(0, c)<<s.substr(c + 1)<<endl;
+    return 0;
+}
